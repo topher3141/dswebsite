0ae74a56804e36968d5fd7dd4f209231e0db2aa0
topher3141 <TOPHER3141@GMAIL.COM>
2026-08-07 17:30:59 -0400

    Update auction-data.ts

--- app/auctions/auction-data.ts
+++ app/auctions/auction-data.ts
@@ -22,6 +22,8 @@ export const auctions: Auction[] = [
     id: "rasmus-r260551",
     title: "Overstock Outdoor Living, Appliances & Home Goods",
     subtitle: "Bid on a wide variety of home goods, small appliances, patio finds, luggage, tech accessories, and more.",
+    auctionLabel: "Online Auction",
+    imageBadge: "Bidding Now Open",
     auctionUrl: "https://www.rasmus.com/auctions/bH7jaBXmN8qTLbHskz2x/a/auction",
     imageUrl: "https://storage.googleapis.com/auction-engine-temp/bH7jaBXmN8qTLbHskz2x/auction-0.jpg",
     closesAt: "2026-08-18T11:07:00-04:00",
